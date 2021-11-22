9c2da5069039a774d9c6a4aecc4ff1ba2165f9f3
iMMoCodes <matti-makela@hotmail.com>
2021-11-22 15:13:55 +0200

    Created welcome page
    -Added mySQL reference library
    -Added JavaFX reference libraries
    -Created images folder and added couple of pictures
    -Created first fxml file -> welcome page/login page
    -Set up code to show the page on editor

--- Application/src/Main.java
+++ Application/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Welcome.fxml"));
+            Scene welcome = new Scene(root);
+            Image icon = new Image("/images/iMMoCodes.png");
+            stage.setResizable(false);
+            stage.getIcons().add(icon);
+            stage.setTitle("Welcome");
+            stage.setScene(welcome);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
